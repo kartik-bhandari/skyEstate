5c4d874fbcf2970e2a2a2b527560ed0466ee959a
kartik bhandari <70146959+kartik-bhandari@users.noreply.github.com>
2024-05-29 15:03:31 +0530

    Update authControllers.js

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -38,7 +38,7 @@ export const signin = async(req,res,next)=>{
         if(!validPassword){
             return next(errorHandler(401, 'incorrect password'))
         }
-        const token = jwt.sign({id: validUser._id} , process.env.JWT_secret) 
+        const token = jwt.sign({id: validUser._id} , "random") 
         const {password : pass , ...rest} = validUser._doc
         res.cookie('access_token',token,{httpOnly:true ,expires: new Date(Date.now( ) + 24*60*60*1000)})
         .status(200)
@@ -52,7 +52,7 @@ export const google = async(req,res,next)=>{
     try {
         const user = await User.findOne({email:req.body.email})
         if(user){
-            const token = jwt.sign({id:user._id} , process.env.JWT_SECRET)
+            const token = jwt.sign({id:user._id} , "random")
             const {password: pass , ...rest} = user._doc
             res.cookie('access_token' , token, {httpOnly: true})
             .status(200)
@@ -63,7 +63,7 @@ export const google = async(req,res,next)=>{
             const hashedPassword = bcryptjs.hashSync(generatedPassword,10)
             const newUser = new User({username: req.body.name.split(" ").join("").toLowerCase() +Math.random().toString(36).slice(-4) , email: req.body.email, password:hashedPassword ,avatar:req.body.photo})
             await newUser.save();
-            const token = jwt.sign({id:newUser._id} , process.env.JWT_SECRET)
+            const token = jwt.sign({id:newUser._id} , "random")
             const {password: pass , ...rest} = user._doc
             res.cookie('access_token' , token, {httpOnly: true})
             .status(200)
