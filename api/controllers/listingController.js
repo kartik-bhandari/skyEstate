21a2267cee4df7ca49a0ad44c8125d7ef6cfc651
kartik bhandari <70146959+kartik-bhandari@users.noreply.github.com>
2024-05-29 20:27:15 +0530

    Update listingController.js

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -91,7 +91,7 @@ export const getListings = async (req,res,next)=>{
 
         const listings = await Listing.find({
             name:{$regex : searchTerm , $options:'i' },
-            address:{$regex : searchTerm , $options:'i' },
+            // address:{$regex : searchTerm , $options:'i' },
             offer,
             furnished,
             parking,
